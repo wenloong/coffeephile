6adf8b983e8bffd1d905bcdd41af8bc948883a9d
Sam Yeo <yeosamuel95@gmail.com>
2021-01-17 03:46:38 -0700

    Add log out/log in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       flash[:success] = "Successfully Logged In!"
-      redirect_to "/users/#{user.id}"
+      redirect_to "/"
     else
       flash[:warning] = "Invalid Username or Password"
       redirect_to '/login'
--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,18 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homepage#index'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
   get '/coffee_beans', to: 'coffee_beans#index'
   get '/specific_bean/:id', to: 'specific_bean#index'
   
-  resources :users, except: [:index] do # view, create, delete, update
-    resources :brews, only: :show
-  end
+   resources :users, except: [:index] do # view, create, delete, update
+     resources :brews, only: :show
+   end
 
   # get '/signup' => 'users#new'
   # post '/users' => 'users#create'
 
-   get '/login' => 'sessions#new'
-   post '/login' => 'sessions#create'
-   get '/logout' => 'sessions#destroy'
 end
