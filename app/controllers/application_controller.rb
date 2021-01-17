89ae333c8db35715af4b2923e2c7b35cf39a688d
Sam Yeo <yeosamuel95@gmail.com>
2021-01-17 08:26:56 -0700

    Ready files for merge

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,5 @@
 class ApplicationController < ActionController::Base
  
-#  def current_user 
-#   @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id] 
-#  end 
-#  helper_method :current_user  
- 
  def authenticate_user! 
   redirect_to '/login' unless current_user 
  end 
