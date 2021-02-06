493666d20d0ac2ef280a20c38a73f7eb7b7b3d42
Sam Yeo <yeosamuel95@gmail.com>
2021-02-06 03:28:00 -0700

    Add Devise method to sign out user to root path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,12 @@
 class ApplicationController < ActionController::Base
  
   def after_sign_in_path_for(resource)
-    dashboard_index_path(current_user) # your path
-  end  
+    dashboard_index_path
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path
+  end
 
  def authenticate_user! 
   redirect_to '/login' unless current_user 
