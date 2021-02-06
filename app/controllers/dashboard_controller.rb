95ba501fdf77c5345853d1b33d4715d4f0ad8a1b
Sam Yeo <yeosamuel95@gmail.com>
2021-02-06 04:00:39 -0700

    Add static view for Dashboard to match wireframe with content

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,7 +3,7 @@ class DashboardController < ApplicationController
   def index
     if !current_user
       flash[:notice] = "You are not logged in!"
-      redirect_to root_path
+      redirect_to new_user_session_path
     end
   end
 end
