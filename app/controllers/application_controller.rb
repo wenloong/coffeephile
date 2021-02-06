94148a6d4a3a4d7cb40e46c52c5953cdb9108f3c
Sam Yeo <yeosamuel95@gmail.com>
2021-02-06 03:23:32 -0700

    Add after_sign_in_path to direct logging in user to dashboard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
  
+  def after_sign_in_path_for(resource)
+    dashboard_index_path(current_user) # your path
+  end  
+
  def authenticate_user! 
   redirect_to '/login' unless current_user 
  end 
