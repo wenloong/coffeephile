c433ae6365481a2d02d034274c2c8c9ed6d52d8a
Sam Yeo <yeosamuel95@gmail.com>
2021-02-06 03:51:14 -0700

    Add functionality to route user to dashboard or root depending if logged in/out

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,9 @@
 class DashboardController < ApplicationController
-
+  
   def index
-
+    if !current_user
+      flash[:notice] = "You are not logged in!"
+      redirect_to root_path
+    end
   end
 end
