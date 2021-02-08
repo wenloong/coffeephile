9cc92be8b669db97dd16ad4b5726f392dcb00c3c
nick-kim-96 <nickthuazn@gmail.com>
2021-02-07 19:10:08 -0800

    added tests for endpoints from homepage

--- tests/local-test.rb
+++ tests/local-test.rb
@@ -2,25 +2,43 @@ require 'selenium-webdriver'
 require 'test-unit'
 
 class LocalTest < Test::Unit::TestCase
-def setup
-  @driver = Selenium::WebDriver.for :firefox
-  @url = "http://localhost:3000/"
-  @driver.manage.timeouts.implicit_wait = 15
-  
- end
+  def setup
+    @driver = Selenium::WebDriver.for :firefox
+    @url = "http://localhost:3000/"
+    @driver.manage.timeouts.implicit_wait = 15
+  end
 
- def test_get_started_button
-  @driver.get(@url)
+  def test_get_home_buttons
+    @driver.get(@url)
 
-  # Search for 
-  get_started_box = @driver.find_element(:xpath, "//input[@type='submit' and @value='Get Started']")
-  #assert_equal(get_started_box.value, "Get Started")
+    wait = Selenium::WebDriver::Wait.new(timeout: 10)
 
-  @driver.action.move_to(get_started_box).click_and_hold
-  sleep 2
+    sleep 2
+    home_header = wait.until {@driver.find_element(:class, "About__info-header")}
+    assert_equal(home_header.text, "The First Largest Coffee Database")
 
-  
-  @driver.quit
- end
+    wait.until {@driver.find_element(:class, "startedButton")}.click
+    sleep 1
+    assert_equal(@driver.current_url, "http://localhost:3000/auth/register/cmon_let_me_in?")
+
+    wait.until {@driver.find_element(:link, "Beans")}.click
+    sleep 1
+    assert_equal(@driver.current_url, "http://localhost:3000/coffee_beans")
+
+    wait.until {@driver.find_element(:link, "Shop")}.click
+    sleep 1
+    assert_equal(@driver.current_url, "http://localhost:3000/shop")
+
+    wait.until {@driver.find_element(:link, "Home")}.click
+    sleep 1
+    assert_equal(@driver.current_url, "http://localhost:3000/")
+
+    wait.until {@driver.find_element(:class, "signButton")}.click
+    sleep 1
+    assert_equal(@driver.current_url, "http://localhost:3000/auth/register/cmon_let_me_in?")
+
+    
+    @driver.quit
+  end
 
 end
